b65dee707b2dbb3db3890e77ae55a097b132fee5
ariellucientec <ariel.garcia@tec.mx>
2020-06-08 12:54:32 -0500

    Session9Update

--- Session9/exceptions.cpp
+++ Session9/exceptions.cpp
@@ -1,24 +1,8 @@
 #include<iostream>
 using namespace std;
 
-class NoFundsException : public exception
-{
-
-public:
-    const char* what()
-    {
-        return "no funds exception";
-    }
-};
 
 int main()
 {
-    try
-    {
-        throw NoFundsException();
-    }
-    catch (NoFundsException& e)
-    {
-        cout << e.what();
-    }
+
 }
